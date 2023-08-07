92bdd50decae385d10c2846fb4fa67701093b908
ionathan sideras <iona8ansideras@gmail.com>
2023-08-07 15:25:25 +0300

    header

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,8 +3,19 @@ import './style.css'
 function App() {
 
   return (
-    <div className='all'>
-
+    <div>
+        <header>
+          <div className='logo'>Logo</div>
+          <nav>
+            <div>Home</div>
+            <div>Skills</div>
+            <div>Projects</div>
+          </nav>
+          <div className='social-links'>
+            <a href='https://www.linkedin.com/in/ionathan-sideras-072a60255' target='_blank'><img src='../public/linkedin.svg'/></a>
+            <a href='https://github.com/ionathansideras' target='_blank'><img src='../public/github.svg'/></a>
+          </div>
+        </header>
     </div>
   )
 }
