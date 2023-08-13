4d52aaae810e8aa9334e11a28a2c791b8ad79338
ionathan sideras <iona8ansideras@gmail.com>
2023-08-13 23:52:15 +0300

    skills

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,9 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import './style.css'
 function App() {
 
   const [info, setInfo] = useState('');
-  const name = 'Hii! My name is Ionathan and im a Frontend Developer'
+  const name = 'Hii! My name is Ionathan and im a Frontend Developer!';
+  const carousel = useRef(null)
+  const [headerBackground, setHeaderBackground] = useState('transparent');
 
   useEffect(()=>{
     let count = 0
@@ -16,13 +18,24 @@ function App() {
       }
     }, 100);
 
-    // in case that it gets unmound we have to clean the interval
-    return () => {clearInterval(timer)}; 
-  },[])
+    const handleScroll = () => {
+      if (window.scrollY !== 0){
+        setHeaderBackground('rgb(17, 16, 16)');
+      } else {
+        setHeaderBackground('transparent')
+      }
+      
+    };
+    
+    window.addEventListener('scroll', handleScroll);
 
+    // in case that it gets unmound we have to clean the interval
+    return () => {clearInterval(timer),window.removeEventListener('scroll', handleScroll);}; 
+  },[])
+  
   return (
     <div>
-        <header>
+        <header style={{backgroundColor: headerBackground}}>
           <div className='logo'>Logo</div>
           <nav>
             <div>Home</div>
@@ -36,12 +49,68 @@ function App() {
         </header>
         <main>
           <div>
+            <div className="welcome">Welcome to my Portfolio</div>
             <h1>{info}</h1>
+            <div className="info">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam odit quia quod repellat, recusandae fuga eligendi modi cumque minus doloremque perspiciatis at asperiores natus quae, deleniti blanditiis consequuntur voluptatum. Ipsa.</div>
           </div>
           <div className="astro-div">
             <img src="../public/astro.svg"/>  
           </div>
         </main>
+        <article>
+          <div className="skills-div">
+            <h1>Skills</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aperiam culpa ratione quod eveniet mollitia nulla. Eos laborum, deserunt</p>
+            <div className="carousel">
+              <h2 className="arrow" onClick ={()=> {
+                carousel.current.scroll({
+                  left: carousel.current.scrollLeft - 130,
+                  behavior: "smooth",
+                })}}>{'<'}</h2>
+              <section ref={carousel}>
+                <div>
+                  <img src="../public/javascript.svg"/>
+                  <p>JavaScript</p>
+                </div>
+                <div>
+                  <img src="../public/react.svg"/>
+                  <p>React</p>
+                </div>
+                <div>
+                  <img src="../public/css-icon.svg"/>
+                  <p>Css</p>
+                </div>
+                <div>
+                  <img src="../public/html-icon.svg"/>
+                  <p>Html</p>
+                </div>
+                <div>
+                  <img src="../public/git-icon.svg"/>
+                  <p>Git</p>
+                </div>
+                <div>
+                  <img src="../public/npm.svg"/>
+                  <p>Npm</p>
+                </div>
+                <div>
+                  <img src="../public/webpack.svg"/>
+                  <p>WebPack</p>
+                </div>
+                <div>
+                  <img src="../public/github-skills.svg"/>
+                  <p>GitHub</p>
+                </div>
+              </section>
+              <h2 className="arrow" onClick={()=> {
+                carousel.current.scroll({
+                  left: carousel.current.scrollLeft + 130,
+                  behavior: "smooth",
+                });
+              }}>{'>'}</h2>
+            </div>
+            
+          </div>
+        </article>
     </div>
   )
 }
