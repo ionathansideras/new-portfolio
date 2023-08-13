202cfe6b75551977318476414a4a8c6d8438a9cd
ionathan sideras <iona8ansideras@gmail.com>
2023-08-13 17:09:50 +0300

    main

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,6 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
 import './style.css'
 function App() {
+
+  const [info, setInfo] = useState('');
+  const name = 'Hii! My name is Ionathan and im a Frontend Developer'
+
+  useEffect(()=>{
+    let count = 0
+    let timer = setInterval(() => {
+      if (count < name.length - 1) {
+        setInfo(prevInfo => prevInfo + name[count]);
+        count++;
+      } else {
+        clearInterval(timer); // Stop the interval when the entire string is added.
+      }
+    }, 100);
+
+    // in case that it gets unmound we have to clean the interval
+    return () => {clearInterval(timer)}; 
+  },[])
 
   return (
     <div>
@@ -16,6 +34,14 @@ function App() {
             <a href='https://github.com/ionathansideras' target='_blank'><img src='../public/github.svg'/></a>
           </div>
         </header>
+        <main>
+          <div>
+            <h1>{info}</h1>
+          </div>
+          <div className="astro-div">
+            <img src="../public/astro.svg"/>  
+          </div>
+        </main>
     </div>
   )
 }
