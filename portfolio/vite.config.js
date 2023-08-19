43074dbb9bc008c654c72d4a6d5bfe124f7ec895
ionathan sideras <iona8ansideras@gmail.com>
2023-08-19 14:20:16 +0300

    test

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: 'https://github.com/ionathansideras/new-portfolio.git',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/react-vite-gh-pages/'
+  }
+
+  return config
 })
